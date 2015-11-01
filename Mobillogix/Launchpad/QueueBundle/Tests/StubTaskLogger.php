3a489c0040e768e903f28455b39cc9b04b74adac
Sergey Ryabenko <sergey@mobillogix.com>
2015-11-01 17:51:56 +0300

    Upload user to BQ and BD

--- Mobillogix/Launchpad/QueueBundle/Tests/StubTaskLogger.php
+++ Mobillogix/Launchpad/QueueBundle/Tests/StubTaskLogger.php
@@ -10,11 +10,13 @@ use Mobillogix\Launchpad\QueueBundle\Service\TaskLoggerInterface;
 class StubTaskLogger implements TaskLoggerInterface
 {
 
+    public $logs = [];
+
     /**
      * @inheritdoc
      */
     public function log(BaseTask $task, $message, $type = self::LOG_MESSAGE)
     {
-
+        $this->logs[] = sprintf('%s [%s]: %s', date('Y-m-d H:i:s'), $type, $message);
     }
 }
